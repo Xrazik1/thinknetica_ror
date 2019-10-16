087eb171d8aa18995a69162914cb7e88e5bb96e2
Nikolay <Xrazik@yandex.ru>
2019-10-16 20:20:34 +0300

    added range with russian characters

--- lesson_2/task_4.rb
+++ lesson_2/task_4.rb
@@ -1,16 +1,10 @@
-# alphabet = ('a'..'я') - забивается не русскими буквами а от латинской "a" до "zz"
+alphabet = ('а'..'я')
 
-alphabet = {
-    "a" => 1, "б" => 2, "в" => 3, "г" => 4, "д" => 5, "е" => 6, "ё" => 7, "ж" => 8, "з" => 9, "и" => 10, "й" => 11, "к" => 12,
-    "л" => 13, "м" => 14, "н" => 15, "о" => 16, "п" => 17, "р" => 18, "c" => 19, "т" => 20, "у" => 21, "ф" => 22, "х" => 23,
-    "ц" => 24, "ч" => 25, "ш" => 26, "щ" => 27, "ъ" => 28, "ы" => 29, "ь" => 30, "э" => 31, "ю" => 32, "я" => 33
-}
-
-vowels = "аоиеёэыуюя"
+vowels = "аоиеэыуюя"
 
 vowel_alphabet = {}
 
-alphabet.each do |letter, position|
+alphabet.each.with_index(1) do |letter, position|
   if vowels.include? letter
     vowel_alphabet[letter] = position
   end
