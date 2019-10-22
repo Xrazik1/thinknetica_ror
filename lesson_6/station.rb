ee19e1bab87f259dce7ee0d598c332c27af0351f
Nikolay <Xrazik@yandex.ru>
2019-10-23 02:57:14 +0300

    Copied lesson_4 folder for 7 task

---
+++ lesson_6/station.rb
@@ -0,0 +1,42 @@
+class Station
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @passenger_trains = []
+    @cargo_trains = []
+  end
+
+  def all_trains
+    @cargo_trains + @passenger_trains
+  end
+
+  def accept_train(train)
+    if train.type == "Грузовой"
+      @cargo_trains << train
+    elsif train.type == "Пассажирский"
+      @passenger_trains << train
+    end
+  end
+
+  def send_train(train)
+    if all_trains.include? train
+      if train.type == "Грузовой"
+        @cargo_trains.delete(train)
+      elsif train.type == "Пассажирский"
+        @passenger_trains.delete(train)
+      end
+      puts "Поезд #{train.number} успешно отправлен со станции: #{@title}"
+    else
+      puts "Поезд с номером #{train.number} отсутствует на станции: #{@title}"
+    end
+  end
+
+  def show_trains
+    all_trains.each { |train| puts " - #{train.number}(#{train.type})" }
+  end
+
+  def show_train_type_quantity
+    puts "Количество поездов на станции '#{@title}': Грузовых - #{@cargo_trains.size}, Пассажирских - #{@passenger_trains.size}"
+  end
+end
