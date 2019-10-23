737f7591fe7080f38c0228e9839c10ea144992ce
Nikolay <Xrazik@yandex.ru>
2019-10-23 22:35:01 +0300

    Included railway error

--- lesson_6/route.rb
+++ lesson_6/route.rb
@@ -1,8 +1,20 @@
+require_relative 'station'
+require_relative 'railway_error'
+
 class Route
   def initialize(start_station, end_station)
     @middle_stations = []
     @start_station = start_station
     @end_station = end_station
+
+    validate!
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
   end
 
   def all_stations
@@ -11,10 +23,10 @@ class Route
 
   def add_station(station)
     if @middle_stations.include? station
-      puts "Станция '#{station.title}' уже есть в маршруте"
+      raise RailwayError.new, "Станция '#{station.title}' уже есть в маршруте"
     else
       @middle_stations << station
-      puts "Станция '#{station.title}' добавлена в маршрут"
+      true
     end
   end
 
@@ -22,16 +34,24 @@ class Route
     if (station != @start_station) && (station != @end_station)
       if @middle_stations.include? station
         @middle_stations.delete(station)
-        puts "Станция '#{station.title}' удалена из маршрута"
+        true
       else
-        puts "Станция '#{station.title}' отсутствует в маршруте"
+        raise RailwayError.new, "Станция '#{station.title}' отсутствует в маршруте"
       end
     else
-      puts "Нельзя изменять начальную и конечную станцию, создайте новый маршрут"
+      raise RailwayError.new, "Нельзя изменять начальную и конечную станцию, создайте новый маршрут"
     end
   end
 
   def show_stations
     all_stations.each { |station| puts station.title }
+  end
+
+  protected
+
+  def validate!
+    raise RailwayError.new, "Начальная станция должна быть объектом класса 'Станция'" unless @start_station.instance_of?(Station)
+    raise RailwayError.new, "Конечная станция должна быть объектом класса 'Станция'" unless @end_station.instance_of?(Station)
+    true
   end
 end
