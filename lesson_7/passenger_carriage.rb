68b7b337dd46119685429c0a421167796cf7c81f
Nikolay <Xrazik@yandex.ru>
2019-10-25 20:18:42 +0300

    Added seats

--- lesson_7/passenger_carriage.rb
+++ lesson_7/passenger_carriage.rb
@@ -1,15 +1,29 @@
+require_relative 'railway_error'
+
 class PassengerCarriage
-  attr_reader :people_quantity
+  attr_reader :seats
 
-  def initialize
-    @people_quantity = 0
+  def initialize(seats)
+    if seats <= 0
+      raise RailwayError.new, "Количество мест в вагоне должно быть больше нуля"
+    else
+      @seats = @vacant_seats = seats
+    end
   end
 
-  def add_passengers(quantity)
-    @people_quantity += quantity
+  def vacant_seats
+    @vacant_seats
   end
 
-  def remove_passengers(quantity)
-    @people_quantity -= quantity
+  def used_seats
+    @seats - @vacant_seats
+  end
+
+  def use_seat
+    if @vacant_seats != 0
+      @vacant_seats -= 1
+    else
+      raise RailwayError.new, "В вагоне отсутствуют свободные места"
+    end
   end
 end
