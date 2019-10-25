d52df997ab42894eb62c607601a93616029dfa63
Nikolay <Xrazik@yandex.ru>
2019-10-25 20:31:25 +0300

    Added some tests

--- lesson_7/test.rb
+++ lesson_7/test.rb
@@ -5,12 +5,12 @@ require_relative 'cargo_carriage'
 require_relative 'passenger_carriage'
 require_relative 'station'
 
-cargo1 = CargoCarriage.new
-cargo2 = CargoCarriage.new
-cargo3 = CargoCarriage.new
-passenger1 = PassengerCarriage.new
-passenger2 = PassengerCarriage.new
-passenger3 = PassengerCarriage.new
+cargo1 = CargoCarriage.new(200)
+cargo2 = CargoCarriage.new(100)
+cargo3 = CargoCarriage.new(200)
+passenger1 = PassengerCarriage.new(10)
+passenger2 = PassengerCarriage.new(10)
+passenger3 = PassengerCarriage.new(30)
 train1 = CargoTrain.new("10A-90")
 train2 = CargoTrain.new("AA110")
 train3 = PassengerTrain.new("111-00")
@@ -21,6 +21,25 @@ station4 = Station.new("Четвертая станция")
 route1 = Route.new(station1, station2)
 route2 = Route.new(station2, station3)
 
+puts "Свободный объем вагона: #{cargo1.free_capacity}"
+puts "Занятый объем вагона: #{cargo1.used_capacity}"
+cargo1.use_capacity(10)
+puts "Свободный объем вагона: #{cargo1.free_capacity}"
+puts "Занятый объем вагона: #{cargo1.used_capacity}"
+
+puts "Количество свободных мест: #{passenger1.vacant_seats}"
+puts "Количество занятых мест: #{passenger1.used_seats}"
+passenger1.use_seat
+passenger1.use_seat
+puts "Количество свободных мест: #{passenger1.vacant_seats}"
+puts "Количество занятых мест: #{passenger1.used_seats}"
+
+train1.add_cargo_carriage(cargo1)
+train1.add_cargo_carriage(cargo2)
+
+train1.iterate_carriages { |carriage| puts "Количество свободного пространства #{carriage.free_capacity}" }
+station1.iterate_trains { |train| puts "Поезда на станции #{train.number}" }
+
 route1.add_station(station3)
 route1.add_station(station4)
 train1.add_route(route1)
@@ -30,7 +49,6 @@ train3.add_route(route1)
 puts "------------------"
 puts "Поезд 100 прицепляет вагон на начальной станции, отвозит на конечную, отцепляет его и возвращается обратно"
 puts "------------------"
-train1.add_cargo_carriage(cargo1)
 train1.increase_speed_by(40)
 train1.move_forward
 train1.move_forward
@@ -62,7 +80,6 @@ train2.increase_speed_by(100)
 train2.move_forward
 train2.stop
 
-
 train2.increase_speed_by(90)
 train2.move_back
 
@@ -73,13 +90,9 @@ puts "
 puts "------------------"
 train3.add_passenger_carriage(passenger1)
 train3.add_passenger_carriage(passenger2)
-train3.add_passengers(passenger1)
-train3.add_passengers(passenger2)
 train3.increase_speed_by(50)
 train3.move_forward
 train3.stop
-train3.remove_passengers(passenger1)
-train3.remove_passengers(passenger2)
 train3.increase_speed_by(50)
 train3.move_forward
 train3.move_forward
