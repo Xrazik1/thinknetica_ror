4e6e2988f4782ac6d90aa272a21ffd81711c88a5
Nikolay <Xrazik@yandex.ru>
2019-10-25 16:43:29 +0300

    copied from lesson_6 directory

---
+++ lesson_7/cargo_train.rb
@@ -0,0 +1,32 @@
+require_relative "train"
+require_relative 'railway_error'
+
+class CargoTrain < Train
+  attr_reader :type
+
+  def initialize(number)
+    super(number)
+    @type = "Грузовой"
+    validate!
+  end
+
+  def add_cargo_carriage(carriage)
+    if @current_speed == 0
+      @carriages.push(carriage)
+    else
+      raise RailwayError.new, "Поезд не может прицеплять вагоны на ходу"
+    end
+  end
+
+  def remove_cargo_carriage(carriage)
+    if @current_speed == 0
+      if @carriages.include?(carriage)
+        @carriages.delete(carriage)
+      else
+        raise RailwayError.new, "У поезда #{number} отсутствуют вагоны"
+      end
+    else
+      raise RailwayError.new, "Поезд не может отцеплять вагоны на ходу"
+    end
+  end
+end
