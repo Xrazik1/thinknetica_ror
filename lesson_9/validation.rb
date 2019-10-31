ae470f02c53d7f590e54b6ff232b987b6260df38
Nikolay <Xrazik@yandex.ru>
2019-10-31 19:18:24 +0300

    Added some fixes

--- lesson_9/validation.rb
+++ lesson_9/validation.rb
@@ -36,27 +36,21 @@ module Validation
         var_value = instance_variable_get(var_name)
         rules.each do |rule|
           check_type = rule[0]
-          params = { value: var_value, rule: rule[1] }
 
-          send("validate_#{check_type}", params)
+          send("validate_#{check_type}", var_value, rule[1])
         end
       end
     end
 
-    def validate_presence(params)
-      value = params[:value]
+    def validate_presence(value, _rule)
       raise RailwayError.new, "Value can't be nil or empty" if value.nil? || value.empty?
     end
 
-    def validate_format(params)
-      value = params[:value]
-      rule = params[:rule]
+    def validate_format(value, rule)
       raise RailwayError.new, "Value doesn't match the format '#{rule}'" if value !~ rule
     end
 
-    def validate_type(params)
-      value = params[:value]
-      rule = params[:rule]
+    def validate_type(value, rule)
       raise RailwayError.new, "Value doesn't match the type '#{rule}'" unless value.instance_of?(rule)
     end
   end
